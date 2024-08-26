4824638be3f0b6b9b4d6615422a7ca2584c8d0e7
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-26 20:42:38 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,6 +1,15 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
+
+//Data Structure for temperatue Limits
+typedef struct{
+int lowerLimit;
+int upperLimit;
+}Temperature_Limit;
+
+Temperature_coolType coolTypeTable[] ={ [PASSIVE_COOLING] = {0, 35}, [HI_ACTIVE_COOLING] = {0, 45}, [MED_ACTIVE_COOLING] = {0, 40} };
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
     return TOO_LOW;
@@ -15,38 +24,22 @@ BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
   int lowerLimit = 0;
   int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
+  Temperature_coolType limts = conditionTable[coolingType]; 
   }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  return inferBreach(temperatureInCelcius, lowerLimit, upperLimit);
 }
 
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
+  if (alertTarget < 0 || alertTarget >= sizeof(alertFunctions) / sizeof(alertFunctions[0])) {
+        printf("Invalid alert target!\n");
+  }
+
   BreachType breachType = classifyTemperatureBreach(
     batteryChar.coolingType, temperatureInC
   );
-
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
+  alertFunctions[alertTarget](breachType);
 }
 
 void sendToController(BreachType breachType) {
@@ -54,18 +47,21 @@ void sendToController(BreachType breachType) {
   printf("%x : %x\n", header, breachType);
 }
 
+//Data Structure for sending email message
+typedef struct {
+    char* message;
+} EmailMessage;
+
+EmailMessage emailMessages[] = {
+    emailMessages[TOO_LOW] = {"Hi, the temperature is too low"},
+    emailMessages[TOO_HIGH] = {"Hi, the temperature is too high"},
+    emailMessages[NORMAL] = {""}
+};
+
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
+  if (emailMessages[breachType].message != NULL) {
+        printf("To: %s\n", recepient);
+        printf("%s\n", emailMessages[breachType].message);
+    }
 }
