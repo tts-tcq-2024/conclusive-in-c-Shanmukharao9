3b4036f44ff4a1739bae7651ffe230c39250f995
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-26 20:51:18 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,69 +1,79 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-//Function pointer for AlertTarget
+// Function pointer typedef for alert functions
 typedef void (*AlertFunction)(BreachType);
 
-//Data Structure for temperatue Limits
-typedef struct{
-int lowerLimit;
-int upperLimit;
-}Temperature_Limit;
-
-Temperature_coolType coolTypeTable[] ={ [PASSIVE_COOLING] = {0, 35}, [HI_ACTIVE_COOLING] = {0, 45}, [MED_ACTIVE_COOLING] = {0, 40} };
-
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
-}
-
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  Temperature_coolType limts = conditionTable[coolingType]; 
-  }
-  return inferBreach(temperatureInCelcius, lowerLimit, upperLimit);
-}
-
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
-  if (alertTarget < 0 || alertTarget >= sizeof(alertFunctions) / sizeof(alertFunctions[0])) {
-        printf("Invalid alert target!\n");
-  }
-
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
-  alertFunctions[alertTarget](breachType);
-}
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
-//Data Structure for sending email message
+// Data Structure for temperature limits
 typedef struct {
-    char* message;
-} EmailMessage;
+    int lowerLimit;
+    int upperLimit;
+} TemperatureLimit;
 
-EmailMessage emailMessages[] = {
-    emailMessages[TOO_LOW] = {"Hi, the temperature is too low"},
-    emailMessages[TOO_HIGH] = {"Hi, the temperature is too high"},
-    emailMessages[NORMAL] = {""}
+// Define the temperature limits for each cooling type
+TemperatureLimit coolTypeTable[] = {
+    [PASSIVE_COOLING] = {0, 35},
+    [HI_ACTIVE_COOLING] = {0, 45},
+    [MED_ACTIVE_COOLING] = {0, 40}
 };
 
+// Infer the breach type based on the temperature and limits
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+    if (value < lowerLimit) {
+        return TOO_LOW;
+    }
+    if (value > upperLimit) {
+        return TOO_HIGH;
+    }
+    return NORMAL;
+}
+
+// Classify the temperature breach based on the cooling type
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    TemperatureLimit limits = coolTypeTable[coolingType];
+    return inferBreach(temperatureInC, limits.lowerLimit, limits.upperLimit);
+}
+
+// Array of alert functions indexed by alert target type
+AlertFunction alertFunctions[] = {
+    [TO_CONTROLLER] = sendToController,
+    [TO_EMAIL] = sendToEmail
+};
+
+// Check and alert based on the temperature and alert target
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    if (alertTarget < 0 || alertTarget >= sizeof(alertFunctions) / sizeof(alertFunctions[0])) {
+        printf("Invalid alert target!\n");
+        return;
+    }
+
+    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    alertFunctions[alertTarget](breachType);
+}
+
+// Function to send alert to the controller
+void sendToController(BreachType breachType) {
+    const unsigned short header = 0xfeed;
+    printf("%x : %x\n", header, breachType);
+}
+
+// Data Structure for sending email message
+typedef struct {
+    const char* message;
+} EmailMessage;
+
+// Define the email messages for each breach type
+EmailMessage emailMessages[] = {
+    [TOO_LOW] = {"Hi, the temperature is too low"},
+    [TOO_HIGH] = {"Hi, the temperature is too high"},
+    [NORMAL] = {""}
+};
+
+// Function to send alert via email
 void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  if (emailMessages[breachType].message != NULL) {
-        printf("To: %s\n", recepient);
+    const char* recipient = "a.b@c.com";
+    if (emailMessages[breachType].message != NULL && emailMessages[breachType].message[0] != '\0') {
+        printf("To: %s\n", recipient);
         printf("%s\n", emailMessages[breachType].message);
     }
 }
